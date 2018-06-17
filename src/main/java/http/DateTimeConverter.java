78ac3869f92898181d057533718dfcdb3c304d16
Ivan Bogachev <fribbis@gmail.com>
2018-06-17 20:54:38 +0300

    Adding of convertDateToString() method to DateTimeConverter class

--- src/main/java/http/DateTimeConverter.java
+++ src/main/java/http/DateTimeConverter.java
@@ -5,19 +5,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateTimeConverter {
-    static Date convertStringToDate(String dateString) {
+    public static Date convertStringToDate(String dateString) {
         Date dateTime = new Date();
         try {
             if (dateString.length() == 10) dateTime = new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
             else if (dateString.length() >= 19) {
                 String[] arrDateTime;
                 arrDateTime = dateString.split("[ T+]");
-                dateTime = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(arrDateTime[0] + " " + arrDateTime[1]);
+                dateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(arrDateTime[0] + " " + arrDateTime[1]);
             }
         }
         catch (ParseException e) {
             e.printStackTrace();
         }
         return dateTime;
+    }
+
+    public static String convertDateToString(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return simpleDateFormat.format(date);
     }
 }
